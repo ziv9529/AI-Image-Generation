8c12e7196aed74995bc7c7caa8f910b5204236f7
ziv9529 <ziva9529@gmail.com>
2023-02-08 12:53:30 +0200

    update server url

--- client/src/services/dalle/index.ts
+++ client/src/services/dalle/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function generatedImageService(prompt: string, count: string): Promise<any> {
-    const { data } = await axios.post('https://ai-image-generation-4aqw.onrender.com/', { prompt: prompt, numOfImage: count });
+    const { data } = await axios.post('https://ai-image-generation-4aqw.onrender.com/api/v1/dalle', { prompt: prompt, numOfImage: count });
     return data.images.data
 }
 
--- client/src/services/posts/index.ts
+++ client/src/services/posts/index.ts
@@ -2,11 +2,11 @@ import axios from "axios";
 import { postModel } from "../../models";
 
 export async function fetchAllPostsService(): Promise<postModel[]> {
-    const { data } = await axios.get('https://ai-image-generation-4aqw.onrender.com/');
+    const { data } = await axios.get('https://ai-image-generation-4aqw.onrender.com/api/v1/post');
     return data.posts
 }
 export async function uploadPostService(form: postModel): Promise<any> {
-    const { data } = await axios.post('https://ai-image-generation-4aqw.onrender.com/', {
+    const { data } = await axios.post('https://ai-image-generation-4aqw.onrender.com/api/v1/post', {
         name: form.name,
         prompt: form.prompt,
         photo: form.photo
